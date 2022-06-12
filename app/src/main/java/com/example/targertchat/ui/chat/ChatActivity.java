b2ae469c1ce9463c3d40c4cce987127a309aff81
Etelis <github@redirecto.me>
2022-06-13 01:34:23 +0300

    Fixed SessionManager, now token will be verified by server. Moved Callback from repository to APIManager.

---
+++ app/src/main/java/com/example/targertchat/ui/chat/ChatActivity.java
@@ -0,0 +1,15 @@
+package com.example.targertchat.ui.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.targertchat.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
