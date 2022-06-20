bd29ad0bd37b38b49a1bc250d61ec8b2faa18b40
orel <orelbenshamai@gmail.com>
2022-06-20 03:19:35 +0300

    s

--- app/src/main/java/com/example/targertchat/ui/user/RegisterActivity.java
+++ app/src/main/java/com/example/targertchat/ui/user/RegisterActivity.java
@@ -1,15 +1,20 @@
 package com.example.targertchat.ui.user;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -20,9 +25,15 @@ import com.example.targertchat.data.utils.PostRegisterUser;
 import com.example.targertchat.ui.contacts.ContactsActivity;
 import com.google.firebase.iid.FirebaseInstanceId;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText userNameEdt, passwordEdt, verifyPasswordEdt, displayNameEdt;
+    private String img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,11 +77,25 @@ public class RegisterActivity extends AppCompatActivity {
                     return;
                 }
 
+                String regex = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,20}";
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(password);
+                if(!matcher.matches()) {
+                    Toast.makeText(RegisterActivity.this, "Password must contain: at least one number, at least one lowercase character, " +
+                            "at least one uppercase character and should be in the length of between 8 to 20" +
+                            "Password can not contain whitespaces!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 if (TextUtils.isEmpty(displayName)) {
                     Toast.makeText(RegisterActivity.this, "Display Name is required!", Toast.LENGTH_SHORT).show();
                 }
 
-                PostRegisterUser registerUser = new PostRegisterUser(userName, password, displayName, null);
+                if (img == null) {
+                    Toast.makeText(RegisterActivity.this, "Image is required!", Toast.LENGTH_SHORT).show();
+                }
+
+                PostRegisterUser registerUser = new PostRegisterUser(userName, password, displayName, img);
 
                 userViewModel.register(registerUser);
                 userViewModel.isLoggedIn().observe(this, answerBoolean -> {
@@ -92,12 +117,35 @@ public class RegisterActivity extends AppCompatActivity {
         }
 
         private void imgBtnInit() {
+            ActivityResultLauncher<Intent> imgLauncher =
+                    registerForActivityResult(
+                            new ActivityResultContracts.StartActivityForResult(), result -> {
+                                if (result.getResultCode() == RESULT_OK && result.getData()!= null) {
+                                    Intent data = result.getData();
+                                    if(data.getData() != null) {
+                                        Uri uri = (Uri) data.getData();
+                                        Bitmap bitmap;
+                                        try {
+                                            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                                            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                                            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+                                            byte[] b = byteArrayOutputStream.toByteArray();
+                                            this.img = Base64.encodeToString(b, Base64.DEFAULT);
+                                        }
+                                        catch(IOException e) {
+                                            Toast.makeText(this,"Error in uploading image", Toast.LENGTH_LONG).show();
+                                        }
+
+                                    }
+                                }
+                            });
+
             Button addImageBtn = findViewById(R.id.addImage);
             addImageBtn.setOnClickListener( view -> {
                         Intent imageBrowser = new Intent(Intent.ACTION_PICK,
                                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                         imageBrowser.setType("image/*");
-                        startActivity(imageBrowser);
+                        imgLauncher.launch(imageBrowser);
                     }
             );
         }
